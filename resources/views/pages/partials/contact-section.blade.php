255a108ba437eccf24d42a21438617cf4b461b81
Jefffrey Davidson <jeffrey.davidson@outlook.com>
2019-10-17 11:51:34 -0400

    Add correct route to contact page

--- resources/views/pages/partials/contact-section.blade.php
+++ resources/views/pages/partials/contact-section.blade.php
@@ -5,7 +5,7 @@
             <div class="col-md-8">
                 <h3>Ready To order Your Project ?</h3>
             </div>
-            <div class="col-md-4"><a href="#" class="btn flat-btn color-btn">Get In Touch</a> </div>
+            <div class="col-md-4"><a href="{{ route('contact') }}" class="btn flat-btn color-btn">Get In Touch</a> </div>
         </div>
     </div>
 </section>
