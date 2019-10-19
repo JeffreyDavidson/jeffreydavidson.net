d500bf358303dbb36f11a6ec7dbf5a1c7a04be5d
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2019-10-18 21:16:38 -0400

    Add migrations

---
+++ database/migrations/2019_10_18_133745_create_post_attachments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostAttachmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_attachments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('post_id');
+            $table->string('name');
+            $table->string('path');
+            $table->boolean('is_thumbnail')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attachments');
+    }
+}
