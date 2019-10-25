f25acdde5cb7849a6be8c2922bebb77c2c7fb651
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2019-10-25 10:47:41 -0400

    Remove typo of additional character

--- resources/views/widgets/recent-posts.blade.php
+++ resources/views/widgets/recent-posts.blade.php
@@ -12,7 +12,7 @@
         <!--article-wrap  -->
         <div class="article-wrap">
             <div class="row">
-                @@foreach($posts as $post)
+                @foreach($posts as $post)
                     <!--article -->
                     <div class="col-md-4">
                         <!-- post -->
